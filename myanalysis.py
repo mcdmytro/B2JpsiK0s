671ee7a38d8361095f1f5c39fc94ce39989c52c9
MELESHKO Dmytro <meleshko@ccw07.cc.kek.jp>
2023-04-03 21:00:37 +0900

    Bremstrahlung correction is implemented.

--- myanalysis.py
+++ myanalysis.py
@@ -21,13 +21,23 @@ ma.inputMdstList(filelist=[b2.find_file(f"starterkit/2021/1111540100_eph3_BGx0_{
 ma.fillParticleList("e+:uncorrected", "electronID > 0.1 and dr < 0.5 and abs(dz) < 2 and thetaInCDCAcceptance", path=main)
 stdV0s.stdKshorts(path=main)
 
+# Bremstrahlung correction
+vm.addAlias("goodFWDGamma", "passesCut(clusterE>0.075 and clusterReg==1)")
+vm.addAlias("goodBRLGamma", "passesCut(clusterE>0.050 and clusterReg==2)")
+vm.addAlias("goodBWDGamma", "passesCut(clusterE>0.100 and clusterReg==3)")
+vm.addAlias("goodGamma", "passesCut(goodBWDGamma or goodBRLGamma or goodBWDGamma)")
+ma.fillParticleList("gamma:brems", "goodGamma", path=main)
+ma.correctBrems("e+:corrected", "e+:uncorrected", "gamma:brems", path=main)
+
 # Combine final state particles to form composite particles
-ma.reconstructDecay("J/psi:ee -> e+:uncorrected e-:uncorrected", cut="dM < 0.11", path=main)
+ma.reconstructDecay("J/psi:ee -> e+:corrected e-:corrected ?addbrems", cut="dM < 0.11", path=main)
 
 # Combine J/psi and KS candidates to form B0 candidates
-dec_str = "B0 -> J/psi:ee K_S0:merged"
-cut_b0 = "Mbc>5.2 and abs(deltaE)<0.15"
-ma.reconstructDecay(dec_str, cut=cut_b0, path=main)
+ma.reconstructDecay(
+    "B0 -> J/psi:ee K_S0:merged",
+    cut="Mbc>5.2 and abs(deltaE)<0.15",
+    path=main
+)
 
 # Perform MC matching
 ma.matchMCTruth("B0", path=main)
@@ -39,28 +49,43 @@ ecl_based_cuts = "thetaInCDCAcceptance and E > 0.05"
 roe_mask = ("my_mask", track_based_cuts, ecl_based_cuts)
 ma.appendROEMasks("B0", [roe_mask], path=main)
 
-###### Define which variable to export to the output file (ntuple)
+#
+#    Define which variable to export to the output file (ntuple)
+#
+
 # vm.addAlias("ep_E","daughter(0, daughter(0, E))") # a way to define an alias for a particular variable
 
-# Variables associated with B0
+# Adding B0 variables
 std_vars = vc.kinematics + vc.mc_kinematics + vc.mc_truth
 all_vars = std_vars + vc.deltae_mbc
 
-# Variables associated with intermediate particles
-interm_vars = vc.inv_mass + std_vars
-all_vars += vu.create_aliases_for_selected(
-    interm_vars, 
-    "B0 -> ^J/psi ^K_S0",
-    prefix=["Jpsi", "K0s"]
-)
+# Add ROE variables
+roe_kinematics = ["roeE()", "roeM()", "roeP()", "roeMbc()", "roeDeltae()"]
+roe_multiplicities = ["nROE_Charged()", "nROE_Photons()", "nROE_NeutralHadrons()"]
+all_vars += roe_kinematics + roe_multiplicities
 
-# Variables associated with charged tracks
-tracks_vars = vc.pid + vc.track + vc.track_hits + std_vars
+# Adding masked ROE variables
+for name in roe_kinematics + roe_multiplicities:
+    all_vars += [name[:-2] + "(my_mask)"]
+
+# Adding final state particles' variables
+fs_vars = vc.pid + vc.track + vc.track_hits + std_vars
+vm.addAlias("isBremsCorrected", "extraInfo(bremsCorrected)")
 all_vars += vu.create_aliases_for_selected(
-    tracks_vars, 
+    fs_vars + ["isBremsCorrected"], 
     "B0 -> [J/psi -> ^e+ ^e-] [K_S0 -> ^pi+ ^pi-]",
     prefix=["ep", "em", "pip", "pim"]
 )
+
+# Adding intermediate particles' variables
+interm_vars = vc.inv_mass + std_vars
+all_vars += vu.create_aliases_for_selected(interm_vars, "B0 -> ^J/psi ^K_S0", prefix=["Jpsi", "K0s"])
+
+# Bremnsstrahlung correction variables
+vm.addAlias("Jpsi_M_uncorrected", "daughter(0, daughterCombination(M,0:0,1:0))")
+vm.addAlias("withBremsCorrection",
+            "passesCut(passesCut(ep_isBremsCorrected == 1) or passesCut(em_isBremsCorrected == 1))")
+all_vars += ["Jpsi_M_uncorrected", "withBremsCorrection"]
 
 # Add variables in CMS for all particles
 cmskinematics = vu.create_aliases(vc.kinematics, "useCMSFrame({variable})", "CMS")
@@ -68,14 +93,6 @@ all_vars += vu.create_aliases_for_selected(
     cmskinematics, 
     "^B0 -> [^J/psi -> ^e+ ^e-] [^K_S0 -> ^pi+ ^pi-]",
     prefix=["B0", "Jpsi", "ep", "em", "K0s", "pip", "pim"])
-
-# Add ROE variables
-roe_kinematics = ["roeE()", "roeM()", "roeP()", "roeMbc()", "roeDeltae()"]
-roe_multiplicities = ["nROE_Charged()", "nROE_Photons()", "nROE_NeutralHadrons()"]
-all_vars += roe_kinematics + roe_multiplicities
-# Adding masked ROE variables
-for name in roe_kinematics + roe_multiplicities:
-    all_vars += [name[:-2] + "(my_mask)"]
 
 # Export reconstruction result in a root file
 ma.variablesToNtuple(
